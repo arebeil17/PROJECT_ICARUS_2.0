e7a1e04b3dcf6eea2bdf88f4ac35c45436c77771
Mike Harmon <mpharmon@email.arizona.edu>
2016-11-30 12:55:39 -0700

    Correcting SAD Routing
    Added a1 and a2 assignments

--- DataMemory/DataMemory.v
+++ DataMemory/DataMemory.v
@@ -59,6 +59,7 @@ module DataMemory(Address, WriteData, ByteSel, Clock, MemWrite, MemRead, ReadDat
         //$readmemh("Labs16-23DM.hex", memory);
         // Lab24DM.hex Initializes Memory for vbsme testing
         //$readmemh("Lab24DM.hex", memory);
+        $readmemh("SAD_DM4x4-2x2.hex", memory);
         // data_memory.txt is for Private Case Testing
         //$readmemh("data_memory.txt", memory);
 //        for(i = 0; i < 256; i = i + 1) begin
